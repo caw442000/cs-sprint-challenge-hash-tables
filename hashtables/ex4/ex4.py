3b6d203b80343e8001c97ae96f0b1c2f9dba43a1
cedric-winbush <cawinbushjr@gmail.com>
2020-07-10 11:54:38 -0500

    completes ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,18 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    numbers = set()
+
+    result = []
+
+    # add all numbers to a set
+    for num in a:
+        numbers.add(num)
+        # if the inverse of the number is in the set
+        # add the positive one to the negatives array
+        # exclude zero
+        if num != 0 and -num in numbers:
+            result.append(abs(num))
 
     return result
 

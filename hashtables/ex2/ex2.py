7636c4ddbe66f526f513c70edbe0c632f5df9b9b
cedric-winbush <cawinbushjr@gmail.com>
2020-07-10 11:58:43 -0500

    adds psuedocode

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,17 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    # initialize route list with the length that is passed in
+
+    # initialize dictionary to hold the tickets for lookup
+
+    # loop through each ticket and store the destination as a value to the source key
+
+    # first ticket has a source of "NONE" -- start there
+
+
+    # look up each ticket's next destination and add it to the route
+
+    
+
     return route

8a75b694b53990666b046a4a96caf4490d3798af
cedric-winbush <cawinbushjr@gmail.com>
2020-07-10 12:12:51 -0500

    MVP completes ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -12,16 +12,25 @@ def reconstruct_trip(tickets, length):
     # Your code here
 
     # initialize route list with the length that is passed in
+    route = [None] * length
 
     # initialize dictionary to hold the tickets for lookup
+    ticket_lookup = {}
 
     # loop through each ticket and store the destination as a value to the source key
+    for ticket in tickets:
+        ticket_lookup[ticket.source] = ticket.destination
 
     # first ticket has a source of "NONE" -- start there
-
+    destination = "NONE"
 
     # look up each ticket's next destination and add it to the route
+    for leg in range(length):
+        # get ticket destination
+        destination = ticket_lookup[destination]
 
-    
+        # add destination to trip 
+        route[leg] = destination
+
 
     return route
